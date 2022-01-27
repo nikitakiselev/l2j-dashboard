675d7918e27a0d16f444a3194792ea3ecf6c087a
Nikita Kiselev <kiselev2008@gmail.com>
2022-01-28 02:44:12 +0300

    feat : Update l2j logo

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -3,11 +3,11 @@
         <NavigationBar/>
 
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <a href="../../index3.html" class="brand-link">
-                <img src="https://adminlte.io/themes/v3/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
-                     class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
-            </a>
+            <Link :href="route('dashboard')" class="brand-link">
+                <img src="/images/L2jServer.png" alt="AdminLTE Logo"
+                     class="brand-image elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">L2J Dashboard</span>
+            </Link>
 
             <div class="sidebar">
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
--- webpack.mix.js
+++ webpack.mix.js
@@ -16,6 +16,7 @@ mix.disableSuccessNotifications();
 mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/css/app.scss', 'public/css')
+    .copy('resources/images', 'public/images')
     .webpackConfig(require('./webpack.config'));
 
 if (mix.inProduction()) {
