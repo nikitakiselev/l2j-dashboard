67abe3e69f16b75c499af2d0b765e15199f5fcca
Nikita Kiselev <kiselev2008@gmail.com>
2022-01-28 02:22:32 +0300

    feat : Add one-click ban account feature

--- resources/js/Pages/Accounts/Index.vue
+++ resources/js/Pages/Accounts/Index.vue
@@ -37,7 +37,8 @@
                 <tr v-for="account in accounts">
                     <td>
                         {{ account.login }}
-                        <span :class="{'badge': true, 'badge-danger': account.accessLevel === -1, 'badge-success': account.accessLevel !== -1}">
+                        <span
+                            :class="{'badge': true, 'badge-danger': account.accessLevel === -1, 'badge-success': account.accessLevel !== -1}">
                             {{ account.accessLevel === -1 ? 'Inactive' : 'Active' }}
                         </span>
                     </td>
@@ -48,7 +49,32 @@
                     <td>{{ account.lastIP }}</td>
                     <td>{{ account.last_game_server?.host }}</td>
                     <td>
-                        <Link :href="route('accounts.edit', account.login)">Edit</Link>
+                        <div class="btn-group">
+                            <Link
+                                :href="route('accounts.edit', account.login)"
+                                class="btn btn-sm btn-default"
+                            >
+                                <i class="fas fa-pen"></i>
+                            </Link>
+
+                            <button
+                                class="btn btn-sm btn-default"
+                                title="Ban account"
+                                v-if="account.accessLevel > -1"
+                                @click.prevent="banAccount(account)"
+                            >
+                                <i class="fa fa-ban"></i>
+                            </button>
+
+                            <button
+                                class="btn btn-sm btn-default"
+                                title="Unlock account"
+                                v-if="account.accessLevel === -1"
+                                @click.prevent="unlockAccount(account)"
+                            >
+                                <i class="fa fa-user-check"></i>
+                            </button>
+                        </div>
                     </td>
                 </tr>
                 </tbody>
@@ -72,7 +98,7 @@ export default {
     },
 
     props: {
-        accounts : {
+        accounts: {
             type: Array,
             default: () => [],
         },
@@ -80,12 +106,22 @@ export default {
 
     methods: {
         formatDate(date) {
-            if (! date) {
+            if (!date) {
                 return '';
             }
 
             return moment(date).format('llll');
-        }
+        },
+
+        banAccount(account) {
+            account.accessLevel = -1;
+            this.$inertia.put(route('accounts.update', account.login), account);
+        },
+
+        unlockAccount(account) {
+            account.accessLevel = 0;
+            this.$inertia.put(route('accounts.update', account.login), account);
+        },
     },
 }
 </script>
